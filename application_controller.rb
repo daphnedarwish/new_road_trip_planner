12943dd951552fca525451b6f72f708d847f8c65
daliad123 <darwish.dalia@gmail.com>
2016-08-19 13:54:00 +0000

    committing2

--- application_controller.rb
+++ application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < Sinatra::Base
   end
   post '/answers' do
     @final_hash = get_sights(params[:state], params[:type])
+    @stops = @final_hash[:stops]
+    @fun = @final_hash[:fun]
+    @img = @final_hash[:img]
+
     erb :answers
   end
 end
--- models/model.rb
+++ models/model.rb
@@ -3,12 +3,12 @@ def get_sights(state, type)
   states = {
       CT:{
         food:{
-          img:"",
+          img:"https://fiftyplates.files.wordpress.com/2014/09/usa-welcome-signs-connecticut-2.jpg",
           stops:["Julio's famous pizza located at 538 Park Ave", "Ola Restaurant located at 350 Boston Post Road"],
           fun:["Mystic Seaport located at 75 Greenmanville Ave", "Foxwoods Resort Casino located at 350 Trolley Line Blvd"],
           },
         art:{
-          img:"",
+          img:"https://fiftyplates.files.wordpress.com/2014/09/usa-welcome-signs-connecticut-2.jpg",
           stops:["II Palio Restaurant located at 5 Corporate Dr", "Luce Restuarant located at 98 Washington st"],
           fun:["Mark Twain House located at 351 Farmington Ave", "Wadsworth Atheneum located at 600 Main St"]
           }
