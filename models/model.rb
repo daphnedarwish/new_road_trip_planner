8fb2ff0a6db8538a48d462727ede793fec0942ec
luciaholzheu <lholzheu@cshnyc.org>
2016-08-19 14:07:07 +0000

    states

--- models/model.rb
+++ models/model.rb
@@ -15,26 +15,38 @@ def get_sights(state, type)
         },
           NY:{
         food:{
-          img:"",
+          img:"https://fiftyplates.files.wordpress.com/2015/05/newyork.jpg",
           stops:["New World Bistro Bar located at 300 Delaware ave", "Yono's Restaurant located at 25 Chapel st"],
           fun:["Great Escape Amusement Park", "Enchanted Forest Water Safari"],
           },
         art:{
-          img:"",
+          img:"https://fiftyplates.files.wordpress.com/2015/05/newyork.jpg",
           stops:["The Frisky Oyster located at 27 Front Street", "The Bear Cafe located at 295 Tinker st"],
           fun:["Niagra Falls", "Golden Hill State Park"]
           }
         },
               MA:{
         food:{
-          img:"",
+          img:"http://blog.mass.gov/wp-content/uploads/2014/07/MA_Welcome_Sign.jpg",
           stops:["Helmand Restaurant located at 143 First st", "Gibbet Hill Grill located at 61 Lowell rd"],
           fun:["TD Garden", "Six Flags"],
           },
         art:{
-          img:"",
+          img:"http://blog.mass.gov/wp-content/uploads/2014/07/MA_Welcome_Sign.jpg",
           stops:["Scampo Restaurant located at 215 Charles st", "Ocean House located at 425 Old Wharf rd"],
           fun:["Plimouth Plantation", "Freedom Trail"]
+          }
+        },
+     VT:{
+        food:{
+          img:"http://cleantechies.com/wp-content/uploads/2014/09/welcome-to-vermont.jpg",
+          stops:["Harrison's Restaurant located at 25 Main St", "A Single Pebble located at 133 Bank St"],
+          fun:["Sugarbush Resort", "Stratton Mountain"],
+          },
+        art:{
+          img:"http://cleantechies.com/wp-content/uploads/2014/09/welcome-to-vermont.jpg",
+          stops:["Sarducci's located at 3 Main St", "Sukho Thai located at 21 Essex Way"],
+          fun:["Shelburne Museum", "Lake Champlain"]
           }
         },
     }
